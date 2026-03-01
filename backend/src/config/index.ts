1a47c2d8b058f05c45de88ee80e5e34b329f49e5
DiogoPereira1230948 <1230948@isep.ipp.pt>
2026-03-01 19:48:37 +0000

    fix: correção no index.ts

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -4,14 +4,13 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
+
 if (envFound.error) {
-    // This error should crash whole process
-
-    throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
-
-if (envFound.error && process.env.NODE_ENV === 'development') {
-    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+    if (process.env.NODE_ENV === 'development') {
+        throw new Error("⚠️  Couldn't find .env file  ⚠️");
+    } else {
+        console.info("ℹ️  No .env file found, using system environment variables.");
+    }
 }
 
 export default {
