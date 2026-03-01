813480101b09dd8a38962e2a2b8d28ac68875f75
DiogoPereira1230948 <1230948@isep.ipp.pt>
2026-03-01 17:37:46 +0000

    fix: otimização de tudo para mobile

--- frontend/src/app/layout/app-layout.component.ts
+++ frontend/src/app/layout/app-layout.component.ts
@@ -1,10 +1,10 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule, RouterOutlet} from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
-import {NotificationsComponent} from './notification/notifications.component';
+import { NotificationsComponent } from './notification/notifications.component';
 
 /**
  * Main application layout component.
@@ -22,4 +22,9 @@ import {NotificationsComponent} from './notification/notifications.component';
 /**
  * App layout component class.
  */
-export class AppLayoutComponent {}
+export class AppLayoutComponent {
+  sidebarOpen = signal(false);
+
+  toggleSidebar() { this.sidebarOpen.update(v => !v); }
+  closeSidebar()  { this.sidebarOpen.set(false); }
+}
